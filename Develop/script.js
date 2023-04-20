072457be71acd773b91dbfd5a60dfdf058ce4115
Allan <allan.santi02@gmail.com>
2023-04-20 12:27:34 -0400

    README EDIT: TODO added to README stating my steps, shall add code in next commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,14 @@
 // Assignment code here
-
+//TODO
+//Create a button to generate a new password.
+//Set up a series of prompts for password criteria.
+//Prompt the user for password length.
+//Validate that password length is between 8 and 128 characters.
+//Prompt the user for which character types to include in the password.
+//Confirm if user wants to include lowercase, uppercase, numeric, and/or special characters.
+//Validate that at least one character type is selected.
+//Generate a password that matches the selected criteria.
+//Display the generated password in an alert or write it to the page.
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
